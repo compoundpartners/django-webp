f34697bb5d671d52d1036b43ea4b738d1377babc
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2021-12-14 12:51:53 +0100

    some fix

--- django_webp/context_processors.py
+++ django_webp/context_processors.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 
 WEBP_VALID_BROWSERS = getattr(settings, 'WEBP_VALID_BROWSERS', ['Chrome', 'Opera', 'Opera Mobile'])
+WEBP_ENABLED = getattr(settings, 'WEBP_ENABLED', True)
 
 
 def _check_by_user_agent(user_agent):
@@ -12,13 +13,13 @@ def _check_by_user_agent(user_agent):
     if user_agent:
         data = httpagentparser.detect(user_agent)
         if 'browser' in data:
-            return data['browser']['name'] in WEBP_VALID_BROWSERS
+            return WEBP_ENABLED and data['browser']['name'] in WEBP_VALID_BROWSERS
 
     return False
 
 
 def _check_by_http_accept_header(http_accept):
-    return 'webp' in http_accept
+    return WEBP_ENABLED and 'webp' in http_accept
 
 
 def webp(request):
--- django_webp/templatetags/webp.py
+++ django_webp/templatetags/webp.py
@@ -13,17 +13,19 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def webp(context, value):
-    converter = WEBPImageConverter()
+    if value:
+        converter = WEBPImageConverter()
 
-    if value.__class__.__name__ == 'Image' and hasattr(value, 'file'):
-        converter.init(value.file.name, value.file.storage, file_obj=value.file)
-    elif value.__class__.__name__ == 'MultiStorageFieldFile':
-        converter.init(value.name, value.storage, file_obj=value.file)
-    else:
-        converter.init(value, staticfiles_storage)
+        if value.__class__.__name__ == 'Image' and hasattr(value, 'file'):
+            converter.init(value.file.name, value.file.storage, file_obj=value.file)
+        elif value.__class__.__name__ == 'MultiStorageFieldFile':
+            converter.init(value.name, value.storage, file_obj=value.file)
+        else:
+            converter.init(value, staticfiles_storage)
 
-    supports_webp = context.get('supports_webp', False)
-    if not supports_webp or WEBP_DEBUG:
-        return converter.get_url()
+        supports_webp = context.get('supports_webp', False)
+        if not supports_webp or WEBP_DEBUG:
+            return converter.get_url()
+        return converter.get_webp_url()
 
-    return converter.get_webp_url()
+    return ''
